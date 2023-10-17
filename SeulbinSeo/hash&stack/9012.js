bddb550e4a5a3a28b2286493c1f2a5ab495fed2a
seoseulbin <80270808+seoseulbin@users.noreply.github.com>
2023-10-18 04:53:52 +0900

    solved coding problems

---
+++ SeulbinSeo/hash&stack/9012.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+//const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync(__dirname + '\\input.txt').toString();
+
+const caseCount = Number(input[0]);
+
+for (let i = 1; i <= caseCount; i += 1) {
+  const cases = input[i];
+  const stack =  [];
+  let result = 'YES';
+
+  for (let j = 0; j < cases.length; j += 1) {
+    if (cases[j] === '(') {
+      stack.push(1);
+    } else {
+      if (!stack.pop()) {
+        result = 'NO';
+        break;
+      } 
+    }
+  }
+
+  if (stack.length !== 0) {
+    result = 'NO';
+  }
+
+  console.log(result);
+}

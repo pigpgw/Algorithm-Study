01778f0269be469c1ad502234d2b1cd7f907b399
KJH910810 <37525284+KJH910810@users.noreply.github.com>
2023-10-18 02:12:56 +0900

    입출력(별찍기16)

---
+++ KimJiHwan/InputOutput/10991.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+let inputData = fs.readFileSync(0, 'utf8').toString();
+
+let a = parseInt(inputData);
+
+for(let i = 1; i <= a; i++){
+    console.log(' '.repeat(a - i) + '* '.repeat(i));
+}
